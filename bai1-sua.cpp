84683af2e60c939226173e5630ac10df83605bbe
Nguyễn Thanh Khả <46096166+NguyenThanhKha@users.noreply.github.com>
2018-12-27 08:29:08 +0700

    Create bai1-sua.cpp

---
+++ bai1-sua.cpp
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+#include<fstream>
+#include<vector>
+#include<iostream>
+using namespace std;
+typedef struct 
+{
+	char ten[20];
+	char sdt[11];
+	char email[20];
+	char diachi[50];
+	bool gioitinh;
+} Contact ;
+//khai bao vector
+vector<Contact> db;
+//khai bao cac thu vien
+void docfile();
+void ghifile();
+void themmoi(Contact c);
+void inContact();
+//doc file .dat
+void docfile()
+{
+	db.clear();
+	FILE *f;
+	f=fopen("danhba.dat","rb");
+	if(f!=NULL)
+	{
+		while(!feof(f))
+		{
+			Contact c;
+			fread(&c, sizeof(Contact),1,f);
+			db.push_back(c);
+		}
+		fclose(f);
+		db.pop_back();
+	}
+}
+// ghi vao file .dat
+void ghifile()
+{
+	FILE*f=fopen("danhba.dat","wb");
+	Contact c;
+	for(int i=0;i<db.size();i++)
+		fwrite(&db[i],sizeof(db),1,f);
+		fclose(f);
+}
+//in danh sach 
+void inContact()
+{
+	for(int i= 0;i<db.size();i++)
+	{
+		cout<<"ten:"<<db[i].ten;
+	}
+}
+// them moi 1 Contact
+void themmoi(Contact c )
+{
+	db.push_back(c);
+	ghifile();
+}
+void chinhsua()
+{
+	
+}
+int main()
+{
+	docfile();
+	Contact c;
+	strcpy(c.ten,"nguyen\n");
+	themmoi(c);
+	Contact c1;
+	strcpy(c1.ten,"uyen");
+	themmoi(c1);
+	inContact();
+	
+}
